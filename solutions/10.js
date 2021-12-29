26c121803aff2136e46ff2ed08efedc6de66946f
Kayla Fortin <kayla.fortin@unity3d.com>
2021-12-29 11:31:59 -0500

    11

--- solutions/10.js
+++ solutions/10.js
@@ -1,5 +1,5 @@
 import { template } from '../helpers/template.js';
-import { isPrime } from '../helpers/utils.js';
+import { getArraySum, getPrimes } from '../helpers/utils.js';
 
 /**
  * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
@@ -13,17 +13,8 @@ const TEST_ARGS = {
 const ARGS = { max: 2000000 };
 
 const solution = ({ max }) => {
-    let primes = []
-    let sum = 0;
-    let j = 1;
-    while (j <= max) {
-        if (isPrime(j, primes)) {
-            primes.push(j);
-            sum += j
-        }
-        j += 1
-    }
-    return sum
+    const primes = getPrimes(1, max);
+    return getArraySum(primes)
 }
 
 template(ARGS, TEST_ARGS, TEST_ANSWER, solution)
