e545e0a9d3dae77b02e0d464805910ea2cee298d
Kayla Fortin <kayla.fortin@unity3d.com>
2022-03-15 11:05:05 -0400

    61

--- helpers/shapes.js
+++ helpers/shapes.js
@@ -3,3 +3,9 @@ export const getTriangleValue = (n) => ((1 / 2) * (n)) * (n + 1)
 export const getPentagonalValue = (n) => (n * (3 * n - 1)) / 2
 
 export const getHexagonalValue = (n) => n * (2 * n - 1)
+
+export const getSquareValue = (n) => n ** 2;
+
+export const getHeptagonalValue = (n) => (n * (5 * n - 3)) / 2;
+
+export const getOctagonalValue = (n) => n * (3 * n - 2);
